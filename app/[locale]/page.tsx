8c12ecaa1a40375ebda05fad5db6ac8d2ec8be55
kira <mohammedalitheiri@gmail.com>
2026-05-21 12:08:53 +0100

    init commit

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Home screen
-    </div>
-  );
+  redirect("/en");
 }
